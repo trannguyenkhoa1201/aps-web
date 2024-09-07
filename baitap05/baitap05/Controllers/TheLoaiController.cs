0e00c4b59638972a0c890f983d146b5be5ca06b5
KHOA <you@example.com>
2024-09-08 01:21:48 +0700

    nop baitap05 len

---
+++ baitap05/baitap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using baitap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngay 12";
+            ViewBag.Thang = "Thang 01";
+            ViewData["nam"] = "nam 2004";
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel 
+            { 
+                Id = 1,
+                Name = "Trinh Tham"
+            };  
+            return View(theloai);
+        }
+    }
+}
---
+++ baitap05/baitap05/Models/TheLoaiViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace baitap05.Models
+{
+    public class TheLoaiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
