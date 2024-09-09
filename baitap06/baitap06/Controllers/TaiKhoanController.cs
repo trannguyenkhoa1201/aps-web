0ebbc63673008bf5d1d4cf77298f098cfc6958ac
KHOA <you@example.com>
2024-09-09 19:38:05 +0700

    nop baitap 06 len

---
+++ baitap06/baitap06/Controllers/TaiKhoanController.cs
@@ -0,0 +1,23 @@
+﻿using baitap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap06.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.Password != null && (taikhoan.Password).Length > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_chuoi_ma_hoa";
+            }
+
+            return View(taikhoan);
+        }
+    }
+}
---
+++ baitap06/baitap06/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace baitap06.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
