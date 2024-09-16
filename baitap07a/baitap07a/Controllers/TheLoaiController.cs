dbd7b04bcf4810ee7abaf9a1a3cac155a4daecc7
KHOA <you@example.com>
2024-09-16 16:59:16 +0700

    nop baitap07a len

---
+++ baitap07a/baitap07a/Controllers/TheLoaiController.cs
@@ -0,0 +1,41 @@
+﻿using baitap07a.Data;
+using baitap07a.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap07a.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var Theloai = _db.TheLoai.ToList();
+            ViewBag.Theloai = Theloai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TheLoai theLoai)
+        {
+            // them thong tin
+            _db.TheLoai.Add(theLoai);
+            // luu thong tin
+            _db.SaveChanges();
+            
+
+            return View();
+        }
+    }
+}
---
+++ baitap07a/baitap07a/Models/TheLoai.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace baitap07a.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
