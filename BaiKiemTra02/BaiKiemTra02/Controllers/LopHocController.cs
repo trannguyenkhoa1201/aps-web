89621b06d5a02773f3363d9c32ae2db46d8bcd48
trannguyenkhoa1201 <trannguyenkhoa121@gmail.com>
2024-09-24 15:20:13 +0700

    nop bai

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,117 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var lopHoc = _db.LopHoc.ToList();
+            ViewBag.LopHoc = lopHoc;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(LopHoc lopHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                // Thêm thông tin vào bảng TheLoai
+                _db.LopHoc.Add(lopHoc);
+                // Lưu lại
+                _db.SaveChanges();
+                // Chuyển trang về index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.LopHoc.Find(id);
+            return View(theloai);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(LopHoc lopHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                // Thêm thông tin vào bảng TheLoai
+                _db.LopHoc.Update(lopHoc);
+                // Lưu lại
+                _db.SaveChanges();
+                // Chuyển trang về index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.LopHoc.Find(id);
+            return View(theloai);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var theloai = _db.LopHoc.Find(id);
+            if (theloai == null)
+            {
+                return NotFound();
+            }
+            _db.LopHoc.Remove(theloai); _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.LopHoc.Find(id);
+            return View(theloai);
+        }
+        [HttpGet]
+        public IActionResult Search(string SearchString)
+        {
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                // Sử dụng LINQ để tìm kiếm
+                var lopHoc = _db.LopHoc.Where(tl => tl.TenLopHoc.Contains(SearchString)).ToList();
+                ViewBag.TheLoai = lopHoc;
+            }
+            else
+            {
+                var lopHoc = _db.LopHoc.ToList();
+                ViewBag.LopHoc = lopHoc;
+            }
+            return View("Index"); // Sử dụng lại View Index
+        }
+
+    }
+}
---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TenLopHoc { get; set; }
+        public int NamNhapHoc { get; set; }
+        public int NamRaTruong { get; set; }
+        public int SoLuongSinhVien { get; set; }
+    }
+}
